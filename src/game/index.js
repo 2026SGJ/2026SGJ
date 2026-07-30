cc3c57e7e68ab445bb85810b201f6d8f765011af
XQYWorld <xqyworld_gh@wpgzs.top>
2026-07-30 00:24:27 +0000

    1

--- src/game/index.js
+++ src/game/index.js
@@ -31,6 +31,7 @@ class Game {
             try {this.players[player].trigger('keyboardEvent', (JSON.parse(event).data));} catch (_){}
         });
         room.onMessage('C2SUpdateRender', ({ who, msg }) => {
+            console.log(`render for ${who.extra.uuid}`, !this.players[who.extra.uuid]);
             const i = this.players[who.extra.uuid];
             if (!i) return;
             render(who.extra.uuid, i.render(this.world.culling));
