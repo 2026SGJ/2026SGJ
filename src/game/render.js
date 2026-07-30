011dda662a46224baa45ed520adb6c5addadf873
XQYWorld <xqyworld_gh@wpgzs.top>
2026-07-30 01:11:36 +0000

    2

--- src/game/render.js
+++ src/game/render.js
@@ -1,19 +1,12 @@
 import room from "../network/index.js";
 
 const render = (playerId, renderData) => {
-    //room.send('S2CClear')
-    for (const i of renderData) {
-        let packet = {
-            dest: playerId,
-            seq: 0,
-            data: i
-        }
-        room.send('S2CRender', JSON.stringify(packet));
-    }
-    room.send('S2CFlush', JSON.stringify({
+    let packet = {
         dest: playerId,
-        seq: 0
-    }));
+        seq: 0,
+        data: renderData
+    }
+    room.send('S2CRender', JSON.stringify(packet));
 };
 
 export default render;
