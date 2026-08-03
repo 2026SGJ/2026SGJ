d584b6396ac30b3bd7526da5076196a481e548ee
XQYWorld <xqyworld_gh@wpgzs.top>
2026-08-03 15:56:55 +0000

    feat: 将渲染函数从单个渲染改为批量渲染

--- src/game/index.js
+++ src/game/index.js
@@ -3,7 +3,7 @@ import playerEvent from '../sessions/index.js';
 import Player from './match/player/index.js';
 import World from './match/world.js';
 import room from '../network/index.js';
-import render from './render.js';
+import renderBatch from './render.js';
 
 /**
  * Game类
