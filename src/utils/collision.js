a4cffbb530beeb71d7326decbf4adfdd6ce115d7
XQYWorld <xqyworld_gh@wpgzs.top>
2026-08-03 02:38:37 +0000

    fix: correct collision detection logic to use player speed for accurate wall interactions

--- src/utils/collision.js
+++ src/utils/collision.js
@@ -1,27 +1,27 @@
 const collisionLeft = (a, b, speed) => {
-    return a.x-speed.x < b.x + b.width &&
-           a.x-speed.x + a.width > b.x &&
+    return a.x+speed.x < b.x + b.width &&
+           a.x+speed.x + a.width > b.x &&
            a.y < b.y + b.height &&
            a.y + a.height > b.y;
 };
 
 const collisionRight = (a, b, speed) => {
-    return a.x-speed.x + a.width > b.x &&
-           a.x-speed.x < b.x + b.width &&
+    return a.x+speed.x + a.width > b.x &&
+           a.x+speed.x < b.x + b.width &&
            a.y < b.y + b.height &&
            a.y + a.height > b.y;
 };
 
 const collisionTop = (a, b, speed) => {
-    return a.y-speed.y < b.y + b.height &&
-           a.y-speed.y + a.height > b.y &&
+    return a.y+speed.y < b.y + b.height &&
+           a.y+speed.y + a.height > b.y &&
            a.x < b.x + b.width &&
            a.x + a.width > b.x;
 };
 
 const collisionBottom = (a, b, speed) => {
-    return a.y-speed.y + a.height > b.y &&
-           a.y-speed.y < b.y + b.height &&
+    return a.y+speed.y + a.height > b.y &&
+           a.y+speed.y < b.y + b.height &&
            a.x < b.x + b.width &&
            a.x + a.width > b.x;
 };
